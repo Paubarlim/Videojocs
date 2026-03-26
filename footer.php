ddcc718559190fbd6a414bfece2ad10f3f724317
a25paubarlim <a25paubarlim@inspedralbes.cat>
2026-03-26 13:44:59 +0100

    modificar el footer

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="px-2 py-2 fixed-bottom bg-dark">
         <span class="text-muted">CRUD de MySQLi con PHP creado por
-            <a class="text-white" href="https://parzibyte.me/blog/">Parzibyte</a>
+            <a class="text-white" href="https://parzibyte.me/blog/">Aplicació Videojocs</a>
             &nbsp;|&nbsp;
             <a target="_blank" class="text-white" href="https://github.com/parzibyte/crud-php-mysqli">
-                Ver código fuente
+                RA5 Digi+Sos
             </a>
         </span>
 </footer>
