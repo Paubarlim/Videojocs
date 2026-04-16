3cb8e53674ae3bf746f83b814de43aba19004819
Paubarlim <a25paubarlim@inspedralbes.cat>
2026-04-16 13:00:22 +0200

    Add 'tipo' column to videojuegos query and display

--- mostrar.php
+++ mostrar.php
@@ -1,16 +1,17 @@
 <?php include_once "header.php"; ?>
 <?php
 $mysqli = include_once "conexion.php";
-$resultado = $mysqli->query("SELECT id, nombre, descripcion FROM videojuegos");
+$resultado = $mysqli->query("SELECT id, nombre, descripcion, tipo FROM videojuegos");
 $videojuegos = $resultado->fetch_all(MYSQLI_ASSOC);?>
- 
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <table class="table">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Nombre</th>
                     <th>Descripción</th>
-                    <th>Categoria</th>
+                    <th>Tipo</th>
                     <th>Editar</th>
                     <th>Eliminar</th>
                 </tr>
@@ -22,7 +23,7 @@ $videojuegos = $resultado->fetch_all(MYSQLI_ASSOC);?>
                         <td><?php echo $videojuego["id"] ?></td>
                         <td><?php echo $videojuego["nombre"] ?></td>
                         <td><?php echo $videojuego["descripcion"] ?></td>
-                        <td><?php echo $videojuego["num"] ?></td>
+                        <td><?php echo $videojuego["tipo"] ?></td>
                         <td>
                             <a href="editar.php?id=<?php echo $videojuego["id"] ?>">Editar</a>
                         </td>
@@ -33,5 +34,7 @@ $videojuegos = $resultado->fetch_all(MYSQLI_ASSOC);?>
                 <?php } ?>
             </tbody>
         </table>
+<button class="btn btn-outline-primary"><a href="index.php">Volver a la página principal</a></button>
+<button class="btn btn-outline-primary"><a href="insertar.php">Añadir Videojuego</a></button>
 
 <?php include_once "footer.php"; ?>
