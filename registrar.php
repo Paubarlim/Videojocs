accb3e6ef2617a58ee936677ae834474f925c0c7
Paubarlim <a25paubarlim@inspedralbes.cat>
2026-04-16 13:01:15 +0200

    Update registrar.php
    Modificar código para poder añadir el parámetro "tipo"

--- registrar.php
+++ registrar.php
@@ -2,10 +2,14 @@
 $mysqli = include_once "conexion.php";
 $nombre = $_POST["nombre"];
 $descripcion = $_POST["descripcion"];
-$sentencia = $mysqli->prepare("INSERT INTO videojuegos
-(nombre, descripcion)
-VALUES
-(?, ?)");
-$sentencia->bind_param("ss", $nombre, $descripcion);
+$tipo = $_POST["tipo"];
+
+$sentencia = $mysql -> prepare("INSERT INTO videojuegos (nombre, descripcion, tipo) VALUES (?, ?, ?)");
+$sentencia->bind_param("ss", $nombre, $descripcion, $tipo);
 $sentencia->execute();
+
+// UN cop fet el INSERT, obtenim l'ID de la nova fila
+$id = mysql->query("SELECT LAST_INSERT_ID()")->fetch_row()[0];
+
+// redigirir a una pág de avís, passant l'id per paràmetre
 header("Location: mostrar.php");
