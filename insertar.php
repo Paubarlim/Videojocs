49e36c0948290d4c27952769098b2f2079cd67b9
Paubarlim <a25paubarlim@inspedralbes.cat>
2026-04-16 12:58:54 +0200

    Add form field for game category type
    Añadir label para poder insertar datos en la nueva columna tipo

--- insertar.php
+++ insertar.php
@@ -1,4 +1,5 @@
 <?php include_once "encabezado.php"; ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <div class="row">
     <div class="col-12">
         <h1>Registrar videojuego</h1>
@@ -11,8 +12,13 @@
                 <label for="descripcion">Descripción</label>
                 <textarea placeholder="Descripción" class="form-control" name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
             </div>
-            <div class="form-group"><button class="btn btn-success">Afegir</button></div>
+            <div class="form-group">
+                <label for="tipo">Tipo</label>
+                <textarea placeholder="Inserte el tipo de categoria del videojuego" class="form-control" name="tipo" id="tipo" required></textarea>
+            </div>
+            <div class="form-group"><button class="btn btn-success">Añadir</button></div>
         </form>
     </div>
 </div>
+<button class="btn btn-dark"><a href="index.php">Volver a la página principal</a></button>
 <?php include_once "footer.php"; ?>
